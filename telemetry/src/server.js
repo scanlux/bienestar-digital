43c116d588ecabd66df698444febbfc6ff7bef7b
harbyscanlux <harbyscanlux@gmail.com>
2026-09-14 20:36:22 -0500

    security(telemetry): restore standard 10mb body parser limit

--- telemetry/src/server.js
+++ telemetry/src/server.js
@@ -33,8 +33,8 @@ app.use(cors({
   credentials: true
 }));
 
-app.use(express.urlencoded({ extended: true, limit: '100mb' }));
-app.use(express.json({ limit: '100mb' }));
+app.use(express.urlencoded({ extended: true, limit: '10mb' }));
+app.use(express.json({ limit: '10mb' }));
 
 const httpServer = createServer(app);
 
